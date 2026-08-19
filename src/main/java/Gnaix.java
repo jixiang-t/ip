9494c116c8d92a7ae24bc996202800ca88caf1d5
Ji Xiang <jixiangtan1@gmail.com>
2026-08-19 16:59:57 +0800

    Level-0 Rename, Greet, Exit

---
+++ src/main/java/Gnaix.java
@@ -0,0 +1,19 @@
+public class Gnaix {
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        String banner = "  ____ _   _    _    _____  __\n"
+                + " / ___| \\ | |  / \\  |_ _\\ \\/ /\n"
+                + "| |  _|  \\| | / _ \\  | | \\  /\n"
+                + "| |_| | |\\  |/ ___ \\ | | /  \\\n"
+                + " \\____|_| \\_/_/   \\_\\___/_/\\_\\";
+
+        System.out.println(separator);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Gnaix");
+        System.out.println("What can I do for you?");
+
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
