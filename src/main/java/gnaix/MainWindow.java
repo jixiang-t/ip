9a1f7f28c77ce91f461ead1a76cacb3838df46d7
Ji Xiang <jixiangtan1@gmail.com>
2026-09-04 11:20:03 +0800

    Polish Gnaix GUI

--- src/main/java/gnaix/MainWindow.java
+++ src/main/java/gnaix/MainWindow.java
@@ -2,9 +2,11 @@ package gnaix;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -12,6 +14,7 @@ import javafx.scene.layout.VBox;
  * Controller for the main Gnaix GUI.
  */
 public class MainWindow extends AnchorPane {
+
     private final Image userImage =
             new Image(getClass().getResourceAsStream("/images/Stewie.png"));
 
@@ -30,12 +33,18 @@ public class MainWindow extends AnchorPane {
     @FXML
     private Button sendButton;
 
+    @FXML
+    private ImageView headerImage;
+
+    @FXML
+    private Label headerBanner;
+
     private Gnaix gnaix;
 
     /**
      * Connects the controller to the Gnaix application.
      *
-     * @param gnaix Gnaix application instance
+     * @param gnaix Gnaix application instance.
      */
     public void setGnaix(Gnaix gnaix) {
         this.gnaix = gnaix;
@@ -46,7 +55,23 @@ public class MainWindow extends AnchorPane {
      */
     @FXML
     public void initialize() {
+        String banner = """
+  ____ _   _    _    _____  __
+ / ___| \\ | |  / \\  |_ _\\ \\/ /
+| |  _|  \\| | / _ \\  | | \\  /
+| |_| | |\\  |/ ___ \\ | | /  \\
+ \\____|_| \\_/_/   \\_\\___/_/\\_\\
+            """;
+
+        headerImage.setImage(gnaixImage);
+        headerBanner.setText(banner);
+
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+
+        dialogContainer.getChildren().add(
+                DialogBox.getGnaixDialog(
+                        "Hello! I'm Gnaix\nWhat can I do for you?",
+                        gnaixImage));
     }
 
     /**
